410e04639b4fa732b6bb6dc40c50c0f743589cd3
Matthew Badcock <matthewlisterb@gmail.com>
2020-03-28 15:50:53 +0100

    added styled components SSR

---
+++ pages/Layout.tsx
@@ -0,0 +1,12 @@
+const Layout = (children) => {
+    return (
+        <>
+            <nav>
+                <h1>Hi!</h1>
+            </nav>
+            <main>{children}</main>
+        </>
+    )
+}
+
+export default Layout
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 const Home = () => <h1>Hello world!</h1>;
 
-    Home.getInitialProps = async ({ req }) => {
-  const userAgent = req ? req.headers['user-agent'] : navigator.userAgent;
-  return { userAgent };
+Home.getInitialProps = async ({ req }) => {
+    const userAgent = req ? req.headers['user-agent'] : navigator.userAgent;
+    return { userAgent }
 };
 
 export default Home;
