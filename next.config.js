e3be372fc7dccf67195190688a250786e7b63c88
Matthew Badcock <matthewlisterb@gmail.com>
2020-04-01 21:45:24 +0200

    service worker

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 const withFonts = require('next-fonts');
+const withOffline = require('next-offline');
 const debug = process.env.NODE_ENV !== 'production';
 
-module.exports = withFonts({
-    target: 'serverless',
-    env: {
-        base_url: 'https://cheapscate7.github.io/json-repo',
-    },
-    webpack(config, options) {
-        return config;
-    },
-});
+module.exports = withOffline(
+    withFonts({
+        target: 'serverless',
+        env: {
+            base_url: 'https://cheapscate7.github.io/json-repo',
+        },
+        webpack(config, options) {
+            return config;
+        },
+    })
+);
